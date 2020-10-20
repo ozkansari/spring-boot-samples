29f44ef69fb9e3e04131296bd9d5fe48b3fcb607
ozkansari <ozkansari@gmail.com>
2020-10-20 11:34:50 +0300

    Small code reorganizations, lambda simplifications

--- spring-boot-embedded-tomcat-ajponly/src/main/java/com/ozkansari/springboot/example/embeddedTomcatAjponly/EmbeddedTomcatAjpOnlyConfigurationApplication.java
+++ spring-boot-embedded-tomcat-ajponly/src/main/java/com/ozkansari/springboot/example/embeddedTomcatAjponly/EmbeddedTomcatAjpOnlyConfigurationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmbeddedTomcatAjpOnlyConfigurationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmbeddedTomcatAjpOnlyConfigurationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmbeddedTomcatAjpOnlyConfigurationApplication.class, args);
+    }
 
 }
--- spring-boot-embedded-tomcat-ajponly/src/main/java/com/ozkansari/springboot/example/embeddedTomcatAjponly/config/EmbeddedTomcatConfig.java
+++ spring-boot-embedded-tomcat-ajponly/src/main/java/com/ozkansari/springboot/example/embeddedTomcatAjponly/config/EmbeddedTomcatConfig.java
@@ -18,76 +18,73 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 public class EmbeddedTomcatConfig {
 
-	@Value("${tomcat.ajp.port}")
-	private int ajpPort;
+    @Value("${tomcat.ajp.port}")
+    private int ajpPort;
+    
+    @Value("${tomcat.ajp.redirectPort:8443}")
+    private int redirectPort;
 
-	@Value("${tomcat.ajp.secret:#{null}}")
-	private String tomcatAjpSecret;
+    @Value("${tomcat.ajp.secret:#{null}}")
+    private String tomcatAjpSecret;
 
-	/**
-	 * The standard protocol value for an AJP connector is AJP/1.3 which uses an
-	 * auto-switching mechanism to select either a Java NIO based connector or an
-	 * APR/native based connector.
-	 */
-	@Value("${tomcat.ajp.protocol:AJP/1.3}")
-	private String tomcatAjpProtocol;
+    /**
+     * The standard protocol value for an AJP connector is AJP/1.3 which uses an
+     * auto-switching mechanism to select either a Java NIO based connector or an
+     * APR/native based connector.
+     */
+    @Value("${tomcat.ajp.protocol:AJP/1.3}")
+    private String tomcatAjpProtocol;
 
-	@Value("${tomcat.ajp.scheme:http}")
-	private String tomcatAjpScheme;
+    @Value("${tomcat.ajp.scheme:http}")
+    private String tomcatAjpScheme;
 
-	@Value("${tomcat.ajp.allowTrace:false}")
-	private boolean tomcatAjpAllowTrace;
-	
-	@Value("${tomcat.ajp.only:true}")
-	private boolean tomcatAjpOnly;
+    @Value("${tomcat.ajp.allowTrace:false}")
+    private boolean tomcatAjpAllowTrace;
+    
+    @Value("${tomcat.ajp.only:true}")
+    private boolean tomcatAjpOnly;
 
-	@Bean
-	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
-		return server -> {
-			if (server instanceof TomcatServletWebServerFactory) {
-				TomcatServletWebServerFactory tomcatServletWebServerFactory = (TomcatServletWebServerFactory) server;
-				if (tomcatAjpOnly) {
-					tomcatServletWebServerFactory.setProtocol(tomcatAjpProtocol);
-					tomcatServletWebServerFactory.setPort(ajpPort);
-					TomcatConnectorCustomizer ajpCustomizer = new TomcatConnectorCustomizer() {
-						@Override
-						public void customize(Connector connector) {
-							initAjpConnector(connector);
-						}
-					};
-					tomcatServletWebServerFactory.getTomcatConnectorCustomizers().add(ajpCustomizer);
-				} else {
-					Connector connector = new Connector(tomcatAjpProtocol);
-					initAjpConnector(connector);
-					tomcatServletWebServerFactory.addAdditionalTomcatConnectors(connector);
-				}
-				
-			}
-		};
-	}
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+        return server -> {
+            if (server instanceof TomcatServletWebServerFactory) {
+                TomcatServletWebServerFactory tomcatServletWebServerFactory = (TomcatServletWebServerFactory) server;
+                if (tomcatAjpOnly) {
+                    tomcatServletWebServerFactory.setProtocol(tomcatAjpProtocol);
+                    tomcatServletWebServerFactory.setPort(ajpPort);
+                    TomcatConnectorCustomizer ajpCustomizer = connector -> initAjpConnector(connector);
+                    tomcatServletWebServerFactory.getTomcatConnectorCustomizers().add(ajpCustomizer);
+                } else {
+                    Connector connector = new Connector(tomcatAjpProtocol);
+                    initAjpConnector(connector);
+                    tomcatServletWebServerFactory.addAdditionalTomcatConnectors(connector);
+                }
+            }
+        };
+    }
 
-	private void initAjpConnector(Connector connector) {
-		connector.setPort(ajpPort);
-		connector.setAllowTrace(tomcatAjpAllowTrace);
-		connector.setScheme(tomcatAjpScheme);
-		AbstractAjpProtocol<? extends Object> ajpProtocol = findAjpProtocol(connector);
-		if (StringUtils.isEmpty(tomcatAjpSecret)) {
-			connector.setSecure(false);
-			ajpProtocol.setSecretRequired(false);
-		} else {
-			connector.setSecure(true);
-			ajpProtocol.setSecret(tomcatAjpSecret);
-		}
-	}
+    private void initAjpConnector(Connector connector) {
+        connector.setPort(ajpPort);
+        connector.setAllowTrace(tomcatAjpAllowTrace);
+        connector.setScheme(tomcatAjpScheme);
+        connector.setRedirectPort(redirectPort);
+        AbstractAjpProtocol<? extends Object> ajpProtocol = findAjpProtocol(connector);
+        if (StringUtils.isEmpty(tomcatAjpSecret)) {
+            connector.setSecure(false);
+            ajpProtocol.setSecretRequired(false);
+        } else {
+            connector.setSecure(true);
+            ajpProtocol.setSecret(tomcatAjpSecret);
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	private AbstractAjpProtocol<? extends Object> findAjpProtocol(Connector connector) {
-		ProtocolHandler protocolHandler = connector.getProtocolHandler();
-		if (protocolHandler instanceof AbstractAjpProtocol) {
-			return (AbstractAjpProtocol<? extends Object>) protocolHandler;
-		}
-		log.error("AJP cannot be configured for {} protocol", protocolHandler.getClass());
-		return null;
-		
-	}
+    @SuppressWarnings("unchecked")
+    private AbstractAjpProtocol<? extends Object> findAjpProtocol(Connector connector) {
+        ProtocolHandler protocolHandler = connector.getProtocolHandler();
+        if (protocolHandler instanceof AbstractAjpProtocol) {
+            return (AbstractAjpProtocol<? extends Object>) protocolHandler;
+        }
+        log.error("AJP cannot be configured for {} protocol", protocolHandler.getClass());
+        return null;    
+    }
 }
